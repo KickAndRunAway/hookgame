5cca4b5607468ed4b9a958e729e40714f6444ded
KickAndRunAway <simonhanak08@gmail.com>
2024-04-23 01:20:19 +0200

    ugh

--- Assets/Door.cs
+++ Assets/Door.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
     private bool triggerActive = false;
-    private bool used = false;
     public SpriteRenderer spriteRenderer;
     public Sprite openSprite;
     public Sprite closedSprite;
+    public ResultScreen ResultScreen;
+    public Timer Timer;
     public Movement Movement;
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -26,9 +28,9 @@ public class Door : MonoBehaviour
         if (Movement.keys == 3)
         {
             spriteRenderer.sprite = openSprite;
-            if (Input.GetKeyDown(KeyCode.Space) && triggerActive)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-
+                ResultScreen.Setup(Timer.timer);
             }
         }
         else
--- Assets/ResultScreen.cs
+++ Assets/ResultScreen.cs
@@ -9,11 +9,9 @@ public class ResultScreen : MonoBehaviour
 {
 
     public TextMeshProUGUI timeText;
-    public Timer Timer;
     public void Setup(float finalTime)
     {
         gameObject.SetActive(true);
         timeText.text = ((int)(finalTime / 600) % 10).ToString() + ((int)(finalTime / 60) % 10).ToString() + ":" + ((int)((finalTime % 60) / 10)).ToString() + ((int)((finalTime % 60) % 10)).ToString() + ":" + ((int)(((finalTime * 100) % 60) / 10)).ToString() + ((int)((finalTime * 100) % 60) % 10).ToString();
     }
-
 }
