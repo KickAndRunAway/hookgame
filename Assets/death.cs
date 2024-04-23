227b12fc4ece34e2ab8d96fdfb5b6db17e53d434
m1kud4yo0 <162576517+m1kud4yo0@users.noreply.github.com>
2024-04-23 13:54:11 +0200

    restart + deathzone

--- Assets/death.cs
+++ Assets/death.cs
@@ -1,13 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class death : MonoBehaviour
 {
-    public BoxCollider2D top;
-    public BoxCollider2D bottom;
-    public BoxCollider2D side;
-    // Start is called before the first frame update
+    //public BoxCollider2D top;
+    //public BoxCollider2D bottom;
+    //public BoxCollider2D side;
+    private bool triggerActive = false;
+
+    private void OnTriggerEnter2D(Collider2D other) //player in der hitbox
+    {
+        triggerActive = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D other) //player nicht in der hitbox
+    {
+        triggerActive = false;
+    }
+    
     void Start()
     {
         
@@ -16,6 +28,14 @@ public class death : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("r") || triggerActive)
+        {
+            Restart();
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
